87b522d1b34d0752939dce4ee9f77ca7f56614c6
Mikhail Elanov <Elanov1991@Gmail.com>
2024-03-09 14:10:48 +0700

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,13 @@ export default function Header() {
     <header>
       <div>
         <span className="logo">Pattaya Tours and Travel</span>
+        <ul className="nav">
+          <li>Главная</li>
+          <li>Экскурсии</li>
+          <li>Такси</li>
+          <li>Бесплатные трансферы</li>
+          <li>Индивидуальные туры</li>
+        </ul>
       </div>
       <div className="presentation"></div>
     </header>
